e14b959c72a82b3a45e71996bac1724e8818b1a1
DevilCyborg <tedshortofficial@gmail.com>
2019-11-09 14:14:27 +0000

    Open keyboard and synth and get receiver and transmitter

--- uSynth/src/USynth.java
+++ uSynth/src/USynth.java
@@ -16,20 +16,34 @@ public class USynth {
                 midiDevices[i] = MidiSystem.getMidiDevice(inf);
                 System.out.println("Position " + i + ": " + inf.getName() + " and " + inf.getDescription());
             } catch (MidiUnavailableException e) {
-                System.err.println("Requested MIDI component cannot be opened or created as it is unavailable.");
+                System.err.println("1 Requested MIDI component cannot be opened or created as it is unavailable.");
             }
            
             i++;
         }
-        MidiDevice keyboard = midiDevices[4];
+        MidiDevice keyboard = midiDevices[1];
+        MidiDevice synth = midiDevices[3];
+        Transmitter t;
+        Receiver r;
         
         if (!(keyboard.isOpen())) {
             try {
                 keyboard.open();
+                t = keyboard.getTransmitter();
             } catch (MidiUnavailableException e) {
-                System.err.println("Requested MIDI component cannot be opened or created as it is unavailable.");
+                System.err.println("2 Requested MIDI component cannot be opened or created as it is unavailable.");
             }
         }
+        
+        if (!(synth.isOpen())){
+            try {
+                synth.open();
+                r = synth.getReceiver();
+            } catch (MidiUnavailableException e) {
+                System.err.println("3 Requested MIDI component cannot be opened or created as it is unavailable.");
+            }
+        }
+        
     }
     
     public static void main(String[] args) {
