b1179d7a68e1e2f4e5c8bf1692d274a06b4c8475
DevilCyborg <tedshortofficial@gmail.com>
2019-11-09 21:03:13 +0000

    CameraFollow and FocusObject Move

---
+++ UnitySynth/Assets/MoveFocusObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFocusObject : MonoBehaviour
+{
+	Rigidbody rb;
+	public float speed = 10f;
+	
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		rb.freezeRotation = false;
+	}
+	
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+		{
+			rb.velocity = new Vector3(.5f, 0, 0) * speed;
+		}
+		
+		if (Input.GetKey(KeyCode.A))
+		{
+			rb.velocity = new Vector3(-.5f, 0, 0) * speed;
+		}
+		
+		if (Input.GetKey(KeyCode.W))
+		{
+			rb.velocity = new Vector3(0, 0, .5f) * speed;
+		}
+		
+		if (Input.GetKey(KeyCode.S))
+		{
+			rb.velocity = new Vector3(0, 0, -.5f) * speed;
+		}
+	}
+}
