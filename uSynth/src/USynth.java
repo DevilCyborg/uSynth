084a45160534c10d6439350441353b50f404ae37
DevilCyborg <tedshortofficial@gmail.com>
2019-11-09 13:53:42 +0000

    Devices are in an array now

---
+++ uSynth/src/USynth.java
@@ -0,0 +1,30 @@
+import javax.sound.midi.*;
+import javax.sound.sampled.*;
+/**
+ *
+ * @author DevilCyborg
+ */
+public class USynth {
+    
+    
+    public USynth() {
+        MidiDevice.Info[] midiInfo = MidiSystem.getMidiDeviceInfo();
+        MidiDevice[] midiDevices = new MidiDevice[midiInfo.length];
+        int i = 0;
+        for (MidiDevice.Info inf : midiInfo){
+            try {
+                midiDevices[i] = MidiSystem.getMidiDevice(inf);
+                System.out.println("Position " + i + ": " + inf.getName() + " and " + inf.getDescription());
+            } catch (MidiUnavailableException e) {
+                System.out.println("Requested MIDI component cannot be opened or created as it is unavailable.");
+            }
+           
+            i++;
+        }
+    }
+    
+    public static void main(String[] args) {
+        USynth us = new USynth();
+    }
+    
+}
