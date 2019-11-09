1bf92800d1416dafdcababbd785b4df7b40ae565
DevilCyborg <tedshortofficial@gmail.com>
2019-11-09 12:44:38 +0000

    Java Project Created, One Class In

---
+++ uSynth/src/uSynth.java
@@ -0,0 +1,15 @@
+import javax.sound.midi.*;
+import javax.sound.sampled.*;
+/**
+ *
+ * @author DevilCyborg
+ */
+public class uSynth {
+
+    public uSynth() {
+        
+    }
+    
+    
+    
+}
