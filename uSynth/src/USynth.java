228006528f63eacdeb5c78dcfab0a13c3fc5e25b
DevilCyborg <tedshortofficial@gmail.com>
2019-11-09 16:25:53 +0000

    Added MidiChannel stuff, swapping to Tym

--- uSynth/src/USynth.java
+++ uSynth/src/USynth.java
@@ -6,56 +6,33 @@ import javax.sound.midi.*;
 public class USynth {
     
     
-    public USynth() {
+    public USynth() throws MidiUnavailableException, InvalidMidiDataException {
         MidiDevice.Info[] midiInfo = MidiSystem.getMidiDeviceInfo();
         MidiDevice[] midiDevices = new MidiDevice[midiInfo.length];
         int i = 0;
         for (MidiDevice.Info inf : midiInfo){
-            try {
-                midiDevices[i] = MidiSystem.getMidiDevice(inf);
-                if (midiDevices[i] instanceof Sequencer){
-                    System.out.print("This is a sequencer -> ");
-                } else if (midiDevices[i] instanceof Synthesizer) {
-                    System.out.print("This is a synthesizer -> ");
-                } else {
-                    System.out.print("What is this? -> ");
-                }
-                System.out.println("Position " + i + ": " + inf.getName() + " and " + inf.getDescription());
-            } catch (MidiUnavailableException e) {
-                System.err.println("1 Requested MIDI component cannot be opened or created as it is unavailable.");
+            midiDevices[i] = MidiSystem.getMidiDevice(inf);
+            if (midiDevices[i] instanceof Sequencer){
+                System.out.print("This is a sequencer -> ");
+            } else if (midiDevices[i] instanceof Synthesizer) {
+                System.out.print("This is a synthesizer -> ");
+            } else {
+                System.out.print("What is this? -> ");
             }
-           
+            System.out.println("Position " + i + ": " + inf.getName() + " and " + inf.getDescription());
             i++;
         }
-        Sequencer seq = null;
-        Synthesizer synth = null;
-        MidiDevice keyboard = midiDevices[4];
-        Transmitter t = null, tport = null;
-        Receiver rsynth = null, rseq = null;
-        try {
-            synth = MidiSystem.getSynthesizer();
-            if (!(synth.isOpen())){
-                synth.open();
-                rsynth = synth.getReceiver();
-            }
-        } catch (MidiUnavailableException e) {
-            System.err.println(e);
+        MidiDevice keyboard = midiDevices[1];
+        Synthesizer synth = MidiSystem.getSynthesizer();
+        if (!(synth.isOpen())){
+            synth.open();
         }
+        Receiver rsynth = synth.getReceiver();
         
-        try {
-            seq = MidiSystem.getSequencer();
-            System.out.println(seq.getDeviceInfo().getName());
-            if (!(seq.isOpen())) {
-                seq.open();
-                t = seq.getTransmitter();
-                rseq = seq.getReceiver();
-                tport = keyboard.getTransmitter();
-            }
-        } catch (MidiUnavailableException e) {
-            System.err.println(e);
-        }
-        t.setReceiver(rsynth);
-        tport.setReceiver(rseq);
+        keyboard.open();
+        Transmitter tport = keyboard.getTransmitter();
+        
+        tport.setReceiver(rsynth);
         
         Instrument[] instruments = synth.getAvailableInstruments();
         for (Instrument instrument : instruments) {
@@ -65,10 +42,18 @@ public class USynth {
         synth.loadInstrument(instruments[9]);
         Soundbank soundbank = instruments[9].getSoundbank();
         System.out.println(synth.isSoundbankSupported(soundbank));
+                
+        MidiChannel[] midichannels = synth.getChannels();
+        midichannels[4].noteOn(60, 93);
     }
     
     public static void main(String[] args) {
-        USynth us = new USynth();
+        try {
+            USynth us = new USynth();
+            
+        } catch (MidiUnavailableException | InvalidMidiDataException e) {
+            System.err.println(e);
+        }
         
     }
     
